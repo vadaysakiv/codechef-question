c2f32fd02e598813eb0623393f04b91d0977defe
VIKAS YADAV <90182273+JoJoHisenburg@users.noreply.github.com>
2021-09-21 16:44:34 +0530

    Update cars_bikes.py

--- cars_bikes.py
+++ cars_bikes.py
@@ -3,11 +3,10 @@ t=int(input("enter the test case no.:"))
 
 for tir in range (1,t+1):
     n=int(input("enter no of tyre which is divisible by 4: "))
-    if n%4==0 and n in range(2,1001):
+    if (n%4==0 or n%4==1) and n in range(2,1001):
         print("NO")
     b=n%4
     if b>1:
         print("YES")
-    else:
-        print("NO")
+    
      
